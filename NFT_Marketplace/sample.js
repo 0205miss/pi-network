4841e062512a6b0ab8424a4cf8c255c555d6c80d
lazytime <74979251+0205miss@users.noreply.github.com>
2021-12-22 10:12:58 +0800

    Update sample.js

--- NFT_Marketplace/sample.js
+++ NFT_Marketplace/sample.js
@@ -1,34 +1,41 @@
 async function nft(){
-  const StellarSdk = require('stellar-sdk');
-  const sourceSecretKey = '';
+  const sourceSecretKey = 'secret key';
   const sourceKeypair = StellarSdk.Keypair.fromSecret(sourceSecretKey);
   const sourcePublicKey = sourceKeypair.publicKey();
   const issuer = StellarSdk.Keypair.random();
-  const server = new StellarSdk.Server('127.0.0.1');
+  const server = new StellarSdk.Server('https://api.testnet.minepi.com/');
   const account = await server.loadAccount(sourcePublicKey);
   const fee = await server.fetchBaseFee();
-  const asset = new StellarSdk.Asset(code,issuer.publicKey())
+  const asset = new StellarSdk.Asset("nft",issuer.publicKey()) //asset type code
   const transaction = new StellarSdk.TransactionBuilder(account,{fee,networkPassphrase:"Pi Testnet"})
-  .addOperation(StellarSdk.Operation.createAccount(
+  .addOperation(StellarSdk.Operation.createAccount({
   destination:issuer.publicKey(),
-  startingBalance: {base reverse*3}
-  ))
-  .addOperation(StellarSdk.Operation.changeTrust(
+  startingBalance: ""  //need base serve * 3
+  }))
+  .addOperation(StellarSdk.Operation.changeTrust({
   asset:asset
-  ))
-  .addOperation(StellarSdk.Operation.payment(
-  destination:sourcePublicKey,
+  }))
+  .addOperation(StellarSdk.Operation.payment({
+  destination:sourceKeypair.publicKey(),
     asset:asset,
-    amount:?,
+    amount:"",//nft amount
     source:issuer.publicKey()
-  ))
-  .addOperation(StellarSdk.Operation.manageData(
+  }))
+  .addOperation(StellarSdk.Operation.manageData({
     name:"ipfs",
-    value:ipfs_hash,
+    value:"",//ipfs_hash
     source:issuer.publicKey()
-  ))
-  .addOperation(StellarSdk.Operation.setOptions(
-    masterWeight:0,
-    homeDomain:?
-  ))
+  }))
+  .addOperation(StellarSdk.Operation.setOptions({
+    masterWeight:"0",
+    homeDomain:"",//stellar.toml host domain
+    source:issuer.publicKey()
+  }))
+  .setTimeout(30)
+  .build();
+  transaction.sign(sourceKeypair);
+  transaction.sign(issuer);
+const transactionResult = await server.submitTransaction(transaction);
+console.log(transactionResult);
+console.log(issuer.publicKey());
 }
